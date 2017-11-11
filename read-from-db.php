32eaa70258fc16745546bc0ba773d75f9480a072
Petros Xen <petros.x12@gmail.com>
2017-11-11 17:13:11 +0000

    dont show id

--- read-from-db.php
+++ read-from-db.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Username: " . $row["username"]. " Score:" . $row["score"]. "<br>";
+        echo "Username: " . $row["username"]. " Score:" . $row["score"]. "<br>";
     }
 } else {
     echo "0 results";
