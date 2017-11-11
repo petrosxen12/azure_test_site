55984a6a083db0345844f7e2413106c4e5979b25
Petros Xen <petros.x12@gmail.com>
2017-11-11 13:12:04 +0000

    added score post req

--- testingReq.php
+++ testingReq.php
@@ -1,8 +1,9 @@
 <?php
 
 $screenname=isset($_GET['username'])?$_GET['username']:"";
-
+$scoreOfUser=isset($_GET['score'])?$_GET['score']:"";
  
 echo  "Username of user is: ".$screenname;
+echo  "Score is: ".$scoreOfUser;
  
 ?>
