3dc7a279b3175c4ac2161a35089043283542ecb9
Petros Xen <petros.x12@gmail.com>
2017-11-11 13:19:00 +0000

    added new line between echoes

--- testingReq.php
+++ testingReq.php
@@ -3,7 +3,7 @@
 $screenname=isset($_GET['username'])?$_GET['username']:"";
 $scoreOfUser=isset($_GET['score'])?$_GET['score']:"";
  
-echo  "Username of user is: ".$screenname;
+echo  "Username of user is: ".$screenname."\n";
 echo  "Score is: ".$scoreOfUser;
  
 ?>
